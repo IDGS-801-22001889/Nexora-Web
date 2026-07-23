ddbe75d4fbebaff2a94e42964ad6dabccf11af9c
IDGS-801-22001889 <barucrangelm@gmail.com>
2026-07-22 21:18:09 -0600

    Login, home temporal, rutas y guards

---
+++ src/app/features/public/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../core/auth.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+  constructor(public authService: AuthService) {}
+}
